2563e773d792aed6f00bae390893da3544ca554a
guangyong.yang <chunming.ygy@gmail.com>
2025-02-19 20:41:01 +1300

    fix dependency issue

---
+++ src/dqn_cartpole.py
@@ -0,0 +1,25 @@
+import gym
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+env = DummyVecEnv([lambda: gym.make("CartPole-v1")])
+
+env = gym.make("CartPole-v1")
+model = DQN("MlpPolicy", env, verbose=1, tensorboard_log="./dqn_cartpole_log/")
+model.learn(total_timesteps=50000)
+
+model.save("dqn_cartpole")
+
+del model
+model = DQN.load("dqn_cartpole")
+
+episodes = 5
+for ep in range(episodes):
+    obs, _ = env.reset()
+    done = False
+    while not done:
+        action, _ = model.predict(obs)
+        obs, reward, done, _, _ = env.step(action)
+        env.render()
+
+env.close()
